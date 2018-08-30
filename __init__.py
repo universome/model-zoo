7a920c0384da4e181d2964170088c6131e3dc934
Ivan Skorokhodov <iskorokhodov@gmail.com>
2018-08-30 18:56:03 +0300

    feat: move general models out from dissonet

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from .ffn import FFN
 from .rnn_classifier import RNNClassifier
 from .rnn_lm import RNNLM
+from .rnn_encoder import RNNEncoder
+from .rnn_decoder import RNNDecoder
 
 __all__ = [
     "FFN",
     "RNNClassifier",
     "RNNLM",
+    "RNNEncoder",
+    "RNNDecoder",
 ]
