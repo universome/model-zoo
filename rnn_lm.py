27fec740a664b5b4816d973adb78770fa68a91a7
Ivan Skorokhodov <iskorokhodov@gmail.com>
2019-02-01 13:24:32 +0300

    feat: move inference into separate module

--- rnn_lm.py
+++ rnn_lm.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from firelab.utils.training_utils import cudable
 
 
 class RNNLM(nn.Module):
@@ -17,25 +16,3 @@ class RNNLM(nn.Module):
         out = self.out(states)
 
         return (out, z) if return_z else out
-
-    def inference(self, x, vocab, max_len=100, eos_token='<eos>'):
-        "Continue sequence given a subsequence"
-        assert x.dim() == 1 # batch_size is 1 and we have only list of token_idx
-
-        result = []
-        x = self.embed(x.unsqueeze(0))
-        z = self.gru(x)[1]
-        current_token = vocab.stoi[eos_token]
-
-        for _ in range(max_len):
-            token = cudable(torch.tensor([[current_token]]))
-            emb = self.embed(token)
-            z = self.gru(emb, z)[1]
-            next_token = self.out(z[:, -1]).max(dim=-1)[1].item()
-
-            result.append(next_token)
-            current_token = next_token
-
-            if next_token == vocab.stoi[eos_token]: break
-
-        return result
